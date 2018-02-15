1c7aef743251e2feac183b06b3b8a6067a03766d
eventsPortal <33833462+eventsPortal@users.noreply.github.com>
2018-02-15 10:35:35 +0300

    Initial update

--- android/src/main/java/com/dineshmaharjan/custommodule/CustomModuleModule.java
+++ android/src/main/java/com/dineshmaharjan/custommodule/CustomModuleModule.java
@@ -6,41 +6,81 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
 import com.facebook.react.bridge.Callback;
-import com.facebook.react.bridge.WritableArray;
-import com.facebook.react.bridge.Arguments;
+
+import android.provider.Settings;
+import android.database.ContentObserver;
+import android.net.Uri;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
 
 public class CustomModuleModule extends ReactContextBaseJavaModule {
 
-    public CustomModuleModule(ReactApplicationContext context) {
-        super(context);
-    }
+  private ReactApplicationContext appContext;
+  private String SYSTEM_ROTATE_PROPERTY = Settings.System.ACCELEROMETER_ROTATION;
 
+  public CustomModuleModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    appContext = reactContext;
+  }
+
+  @Override
+  public String getName() {
+    return "CustomModule";
+  }
+
+  private ContentObserver _autoRotationSettingObserver = new AutoRotationSettingObserver();
+
+  private class AutoRotationSettingObserver extends ContentObserver {
+    public AutoRotationSettingObserver() {
+      super(null);
+    }
     @Override
-    public String getName() {
-        return "CustomModule";
+    public void onChange(boolean selfChange) {
+      this.onChange(selfChange, null);
     }
-
-    @ReactMethod
-    public void getModuleList(final Callback callback) {
-        try {
-
-            WritableArray list = getList();
-            callback.invoke(null, list);
-
-        } catch (Exception e) {
-            callback.invoke("Error", null);
-        }
+    @Override
+    public void onChange(boolean selfChange, Uri uri) {
+      boolean autoRotationSetting = getAutoRotationSetting();
+      appContext
+        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+        .emit("autoRotationSettingChanged", autoRotationSetting);
     }
+  }
 
-
-   private WritableArray getList() {
-
-        WritableArray listArr = Arguments.createArray();
-        listArr.pushString("react-native-fbsdk");
-        listArr.pushString("react-native-camera");
-        listArr.pushString("react-native-map");
-
-        return listArr;
+  public boolean getAutoRotationSetting() {
+    try {
+      String ROTATE_PROP = Settings.System.ACCELEROMETER_ROTATION;
+      boolean autoRotateSettingValue = Settings.System.getInt(appContext.getContentResolver(), ROTATE_PROP) != 0;
+      return autoRotateSettingValue;
+    } catch (Exception e) {
+      return false;
     }
+  }
+
+
+  @ReactMethod
+  public void init() {
+    try {
+      appContext.getContentResolver().registerContentObserver(
+        Settings.System.getUriFor(SYSTEM_ROTATE_PROPERTY),
+        false,
+        _autoRotationSettingObserver
+      );
+    } catch(Exception e) {}
+  }
+
+  @ReactMethod
+  public void remove() {
+    try {
+      appContext.getContentResolver().unregisterContentObserver(_autoRotationSettingObserver);
+    } catch(Exception e) {}
+  }
+
+  @ReactMethod
+  public void getAutoRotationValue(Callback callback) {
+    boolean autoRotateSettingValue = getAutoRotationSetting();
+    callback.invoke(autoRotateSettingValue);
+  }
+
 
 }
